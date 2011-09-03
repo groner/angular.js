dae4b97c947cbf4cb5fcc53cc74d7a0ce7faf67f
Igor Minar <igor@angularjs.org>
2011-09-02 22:59:59 -0700

    fix(ng:options): ng:change should be called after the new val is set
    Closes #547

--- src/widgets.js
+++ src/widgets.js
@@ -765,8 +765,8 @@ angularWidget('select', function(element){
           }
         }
         if (isDefined(value) && model.get() !== value) {
-          onChange(scope);
           model.set(value);
+          onChange(scope);
         }
         scope.$tryEval(function(){
           scope.$root.$eval();
